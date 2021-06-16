717b57ec3f7d23137b2bdd431fc06314734ce4b7
kohsin <943280106@qq.com>
2021-06-16 17:43:01 +0800

    封装配置文件读取类

--- spark-realtime/src/main/scala/spark/realtime/utils/MyPropertiesUtil.scala
+++ spark-realtime/src/main/scala/spark/realtime/utils/MyPropertiesUtil.scala
@@ -1,5 +1,21 @@
 package spark.realtime.utils
 
-object MyPropertiesUtil {
+import java.io.{FileInputStream, InputStreamReader}
+import java.nio.charset.StandardCharsets
+import java.util.Properties
 
+/**
+ * properties 读取配置文件
+ */
+object MyPropertiesUtil {
+  def load(propertiesName: String): Properties = {
+    val prop = new Properties()
+    // 加载执行的配置文件
+    // 编译之后放在class下面了
+    // 用类加载器来读取
+    prop.load(new InputStreamReader(
+      Thread.currentThread().getContextClassLoader.getResourceAsStream(propertiesName),
+      StandardCharsets.UTF_8))
+    prop
+  }
 }
