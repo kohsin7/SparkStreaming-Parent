dbed97febd1b6d2c680b613ef065e428cadc90a0
kohsin <943280106@qq.com>
2021-06-22 15:36:38 +0800

    保存到ES之后 将偏移量保存到Redis中

--- spark-realtime/src/main/scala/spark/realtime/utils/OffsetManagerUtil.scala
+++ spark-realtime/src/main/scala/spark/realtime/utils/OffsetManagerUtil.scala
@@ -3,8 +3,10 @@ package spark.realtime.utils
 import java.util
 
 import org.apache.kafka.common.TopicPartition
+import org.apache.spark.streaming.kafka010.OffsetRange
 
 object OffsetManagerUtil {
+
   // 从 redis 中获取 offset  type:hash    key:offset:topic:groupid    field:partition   value:偏移量
   def getOffset(topic: String, groupId: String): Map[TopicPartition, Long] = {
     // 获取客户端连接
@@ -30,4 +32,22 @@ object OffsetManagerUtil {
   }
 
   // 将 offset 信息保存到 Redis 中
+  def saveOffset(topic: String, groupId: String, offsetRanges: Array[OffsetRange]): Unit = {
+    var offsetKey = "offset:" + topic + ":" + groupId
+    // 定义一个 java 的 map 集合，用于存放每个分区对应的偏移量
+    val offsetMap = new util.HashMap[String, String]()
+    val jedis = MyRedisUtil.getJedisClient()
+
+    // 对 offsetRanges 遍历，将数据封装到 offsetMap 中
+    for (offsetRange <- offsetRanges) {
+      val partitionId: Int = offsetRange.partition
+      val fromOffset: Long = offsetRange.fromOffset
+      val untilOffset: Long = offsetRange.untilOffset
+      offsetMap.put(partitionId.toString, untilOffset.toString)
+      println("保存分区" + partitionId + ":" + fromOffset + "---->" + untilOffset)
+    }
+
+    jedis.hmset(offsetKey, offsetMap)
+    jedis.close()
+  }
 }
